7dedda281454e9670245bae83ebf4b4930ee2e39
Your Name <you@example.com>
2021-03-25 10:02:52 +0100

    modified:   bot.rb

--- bot.rb
+++ bot.rb
@@ -29,9 +29,22 @@ while(1==1)
 		    	ssl.puts "PRIVMSG #{channel} https://nitter.cc#{message}"
 		end
 
-   elsif line.include? ":p#"
+
+	elsif line.include? ":t@"
         channel=line[line.index(' #')+1..line.index(' :')-1]
-        searchterm=line[line.index(':p#')+3..line.length].chomp
+        searchterm=line[line.index(':t@')+3..line.length].chomp
+	        if sanitizeinput(searchterm)
+   	        	if `curl -Ns https://nitter.cc/#{searchterm}/media`.=~ /not found|No items found|suspended/
+		        	ssl.puts "PRIVMSG #{channel} :Sorry, got nothing!"
+		        	next			
+		        end
+				message= `curl -Ns "https://nitter.cc/#{searchterm}/media" | grep -E 'tweet-content media-body.+\/div>' |sed -E 's\/.*auto">|<\\/div>|<div>|<\\/a>\/\/g' |sed -E 's\/<a href=".*>\/\/g'|shuf -n1`
+				puts 	 message
+		        ssl.puts "PRIVMSG #{channel} :@#{searchterm}: #{message}"      
+			end	
+   elsif line.include? ":p~"
+        channel=line[line.index(' #')+1..line.index(' :')-1]
+        searchterm=line[line.index(':p~')+3..line.length].chomp
         if sanitizeinput(searchterm)
 	        if `curl -Ns "https://nitter.cc/search?f=tweets\&q=#{searchterm}" | grep -Eo 'No items found'`.include?"No items found" or not(`curl -Ns "https://nitter.cc/search?f=tweets\&q=#{searchterm}" | grep -Eo "src=\\"/pic/media.+(jpg|png)" | sed -E "s/src=\\"//" | shuf -n1`=~/jpg|png/)
    			    ssl.puts "PRIVMSG #{channel} :Sorry, got nothing!"
@@ -42,9 +55,9 @@ while(1==1)
 		        ssl.puts "PRIVMSG #{channel} https://nitter.cc#{message}"
 		end		
 
-	elsif line.include? ":t#"
+	elsif line.include? ":t~"
 	        channel=line[line.index(' #')+1..line.index(' :')-1]
-	        searchterm=line[line.index(':t#')+3..line.length].chomp
+	        searchterm=line[line.index(':t~')+3..line.length].chomp
 	        if sanitizeinput(searchterm)
 	        if `curl -Ns "https://nitter.cc/search?f=tweets\&q=#{searchterm}" | grep -Eo "No items found"`.include?"No items found"
 	           		 ssl.puts "PRIVMSG #{channel} :Sorry, got nothing!"
@@ -54,28 +67,39 @@ while(1==1)
 					puts 	 message
 			        ssl.puts "PRIVMSG #{channel} :#{message}"
 			end	
+ elsif line.include? ":p#"
+        channel=line[line.index(' #')+1..line.index(' :')-1]
+        searchterm=line[line.index(':p#')+3..line.length].chomp
+        if sanitizeinput(searchterm)
+	        if `curl -Ns "https://nitter.cc/search?f=tweets\&q=%23#{searchterm}" | grep -Eo 'No items found'`.include?"No items found" or not(`curl -Ns "https://nitter.cc/search?f=tweets\&q%23=#{searchterm}" | grep -Eo "src=\\"/pic/media.+(jpg|png)" | sed -E "s/src=\\"//" | shuf -n1`=~/jpg|png/)
+   			    ssl.puts "PRIVMSG #{channel} :Sorry, got nothing!"
+			    next
+	        end
+				message= `curl -Ns "https://nitter.cc/search?f=tweets\&q=%23#{searchterm}" | grep -Eo "src=\\"/pic/media.+(jpg|png|mp4)" | sed -E "s/src=\\"//" | shuf -n1`
+				puts 	 message
+		        ssl.puts "PRIVMSG #{channel} https://nitter.cc#{message}"
+		end		
 
-	elsif line.include? ":t@"
-		        channel=line[line.index(' #')+1..line.index(' :')-1]
-		        searchterm=line[line.index(':t@')+3..line.length].chomp
-		        if sanitizeinput(searchterm)
-    	        	if `curl -Ns https://nitter.cc/#{searchterm}/media`.=~ /not found|No items found|suspended/
-			        	ssl.puts "PRIVMSG #{channel} :Sorry, got nothing!"
-			        	next			
-			        end
-						message= `curl -Ns "https://nitter.cc/#{searchterm}/media" | grep -E 'tweet-content media-body.+\/div>' |sed -E 's\/.*auto">|<\\/div>|<div>|<\\/a>\/\/g' |sed -E 's\/<a href=".*>\/\/g'|shuf -n1`
-						puts 	 message
-				        ssl.puts "PRIVMSG #{channel} :@#{searchterm}: #{message}"      
-			end	
+	elsif line.include? ":t#"
+	        channel=line[line.index(' #')+1..line.index(' :')-1]
+	        searchterm=line[line.index(':t#')+3..line.length].chomp
+	        if sanitizeinput(searchterm)
+	        if `curl -Ns "https://nitter.cc/search?f=tweets\&q=%23#{searchterm}" | grep -Eo "No items found"`.include?"No items found"
+	           		 ssl.puts "PRIVMSG #{channel} :Sorry, got nothing!"
+	   			    next
+   	        end
+						message= `curl -Ns "https://nitter.cc/search?f=tweets\&q=%23#{searchterm}"| grep -E 'tweet-content media-body.+\/div>' |sed -E 's\/.*auto">|<\\/div>|<div>|<\\/a>\/\/g' |sed -E 's\/<a href=".*>\/\/g'|shuf -n1`
+					puts 	 message
+			        ssl.puts "PRIVMSG #{channel} :#{message}"
+			end
 
     elsif line.include? "PING"
         ssl.puts "PONG #{line[line.index(':')+1..line.length]}"
         puts "PONG #{line[line.index(':')+1..line.length]}"
 		sleep(1)
-
     elsif line.include? ":.bots"
     		        channel=line[line.index(' #')+1..line.index(' :')-1]
-          ssl.puts "PRIVMSG #{channel} :Reporting in! [Ruby] https://github.com/Azrotronik/NitterBot, commands are t@,t#,p@,p#"
+          ssl.puts "PRIVMSG #{channel} :Reporting in! [Ruby] https://github.com/Azrotronik/NitterBot, commands are t@,t#,t~,p@,p#,p~"
 
 	elsif line.include? "VERSION"
 	     ssl.puts "VERSION NitterBot"
